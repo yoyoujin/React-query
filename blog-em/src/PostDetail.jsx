8b640abbbeed0293d18f2cde9db8ffcb649ee26f
youjin <yjinhann@gmail.com>
2023-06-15 17:18:17 +0900

    feature: comments query

--- blog-em/src/PostDetail.jsx
+++ blog-em/src/PostDetail.jsx
@@ -1,33 +1,43 @@
+import { useQuery } from '@tanstack/react-query';
+
 async function fetchComments(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
-  );
+  const response = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`);
   return response.json();
 }
 
 async function deletePost(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "DELETE" }
-  );
+  const response = await fetch(`https://jsonplaceholder.typicode.com/postId/${postId}`, {
+    method: 'DELETE',
+  });
   return response.json();
 }
 
 async function updatePost(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } }
-  );
+  const response = await fetch(`https://jsonplaceholder.typicode.com/postId/${postId}`, {
+    method: 'PATCH',
+    data: { title: 'REACT QUERY FOREVER!!!!' },
+  });
   return response.json();
 }
 
 export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
+  const { data, isLoading, isError, error } = useQuery(['comments', post.id], () =>
+    fetchComments(post.id)
+  );
+
+  if (isLoading) return <h3>Loading...</h3>;
+
+  if (isError)
+    return (
+      <>
+        <h3>Error</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
 
   return (
     <>
-      <h3 style={{ color: "blue" }}>{post.title}</h3>
+      <h3 style={{ color: 'blue' }}>{post.title}</h3>
       <button>Delete</button> <button>Update title</button>
       <p>{post.body}</p>
       <h4>Comments</h4>
@@ -39,3 +49,26 @@ export function PostDetail({ post }) {
     </>
   );
 }
+
+// Why don't commets refresh?
+// - Every query uses the same key (comments)
+// - Data for queries with known keys only refetched upon trigger
+// - Example triggers:
+//   - component remount
+//   - window focus
+//   - running refetch function
+//   - automated refetch
+//   - query invalidation after a mutation
+
+// Solution?
+// - Option: remove programmatically for every new title
+//   - it's not easy
+//   - it's not really what we want
+// - No reason to remove data from the cache
+//   - we're not even perfoming the same query!
+//   - 블로그 게시물 1을 클릭했을 때 게시글 1에 대한 캐시데이터를 사용하는 것이 좋지, 게시물 2의 댓글로 덮어쓰는 것은 좋지 않음!
+// - Query includes postID
+//   - Cache on a per-query basis
+//   - don't share cache for any 'comments' query regardless of post id
+// - What we really want: label the query for each post separately
+// 👉 배열에 두번째 아이템으로 postid를 함께 넘겨줘야함! [] : 쿼리에 대한 의존성 배열
