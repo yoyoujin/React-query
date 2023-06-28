f5e65fa7cc27911f2d397ef7a75021646f21ecd3
youjin <yjinhann@gmail.com>
2023-06-28 21:59:54 +0900

    feature: 페이지네이션

--- blog-em/src/Posts.jsx
+++ blog-em/src/Posts.jsx
@@ -4,17 +4,25 @@ import { PostDetail } from './PostDetail';
 
 const maxPostPage = 10;
 
-async function fetchPosts() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0');
+async function fetchPosts(pageNum) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
+  );
   return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const { data, isLoading, isError, error } = useQuery(['posts'], fetchPosts, { staleTime: 2000 });
+  const { data, isLoading, isError, error } = useQuery(
+    ['posts', currentPage],
+    () => fetchPosts(currentPage),
+    {
+      staleTime: 2000,
+    }
+  );
   // 블로그 게시물이 2초마다 만료되도록 설정함
 
   if (isLoading) return <h3>Loading...</h3>;
@@ -31,17 +39,31 @@ export function Posts() {
     <>
       <ul>
         {data.map((post) => (
-          <li key={post.id} className='post-title' onClick={() => setSelectedPost(post)}>
+          <li
+            key={post.id}
+            className='post-title'
+            onClick={() => setSelectedPost(post)}
+          >
             {post.title}
           </li>
         ))}
       </ul>
       <div className='pages'>
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage((previousValue) => previousValue - 1);
+          }}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage((previousValue) => previousValue + 1);
+          }}
+        >
           Next page
         </button>
       </div>
